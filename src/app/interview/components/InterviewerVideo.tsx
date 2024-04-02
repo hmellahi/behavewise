3b4bbf7b82270e184d88e8172f416c0148081013
hmellahi <hamzasenpai307@gmail.com>
2024-04-02 01:55:06 +0000

    refactoring

--- src/app/interview/components/InterviewerVideo.tsx
+++ src/app/interview/components/InterviewerVideo.tsx
@@ -1,26 +1,28 @@
 import { forwardRef } from "react";
 
-const InterviewerVideo = forwardRef(({ setVideoEnded }, ref) => {
-  return (
-    <div className="block absolute top-[10px] sm:top-[20px] lg:top-[40px] left-auto right-[10px] sm:right-[20px] md:right-10 h-[80px] sm:h-[140px] md:h-[180px] aspect-video rounded z-20">
-      <div className="h-full w-full aspect-video rounded md:rounded-lg lg:rounded-xl">
-        <video
-          id="question-video"
-          onEnded={() => setVideoEnded(true)}
-          controls={false}
-          ref={ref}
-          playsInline
-          className="h-full object-cover w-full rounded-md md:rounded-[12px] aspect-video"
-          crossOrigin="anonymous"
-        >
-          <source
-            src="https://liftoff-public.s3.amazonaws.com/DemoInterviewMale.mp4"
-            type="video/mp4"
-          />
-        </video>
+const InterviewerVideo = forwardRef(
+  ({ onVideoEnded }: { onVideoEnded: () => void }, ref) => {
+    return (
+      <div className="block absolute top-[10px] sm:top-[20px] lg:top-[40px] left-auto right-[10px] sm:right-[20px] md:right-10 h-[80px] sm:h-[140px] md:h-[180px] aspect-video rounded z-20">
+        <div className="h-full w-full aspect-video rounded md:rounded-lg lg:rounded-xl">
+          <video
+            id="question-video"
+            onEnded={() => onVideoEnded()}
+            controls={false}
+            ref={ref}
+            playsInline
+            className="h-full object-cover w-full rounded-md md:rounded-[12px] aspect-video"
+            crossOrigin="anonymous"
+          >
+            <source
+              src="https://liftoff-public.s3.amazonaws.com/DemoInterviewMale.mp4"
+              type="video/mp4"
+            />
+          </video>
+        </div>
       </div>
-    </div>
-  );
-});
+    );
+  }
+);
 
 export default InterviewerVideo;
--- src/app/interview/page.tsx
+++ src/app/interview/page.tsx
@@ -2,14 +2,9 @@
 import SuccessButton from "@/components/common/SuccessButton";
 import { RightArrow } from "@/components/svgs";
 import Spinner from "@/components/svgs/Spinner";
-import interviewers from "@/constants/interviewers";
-import questions from "@/constants/questions";
-import { ffmpeg } from "@/plugins/ffmpeg";
-import { fetchFile } from "@ffmpeg/ffmpeg";
 import { AnimatePresence, motion } from "framer-motion";
 import { useCallback, useEffect, useRef, useState } from "react";
 import Webcam from "react-webcam";
-import { v4 as uuid } from "uuid";
 import InterviewerVideo from "./components/InterviewerVideo";
 import LoadingState from "./components/LoadingState";
 import NoCameraAccess from "./components/NoCameraAccess";
@@ -19,11 +14,7 @@ import StartTimerButton from "./components/ui/StartTimerButton";
 import StopTimerButton from "./components/ui/StopTimerButton";
 import Timer from "./components/ui/Timer";
 
-export default function DemoPage() {
-  const [selected, setSelected] = useState(questions[0]);
-  const [selectedInterviewer, setSelectedInterviewer] = useState(
-    interviewers[0]
-  );
+export default function Interview() {
   const [loading, setLoading] = useState(true);
   const webcamRef = useRef<Webcam | null>(null);
   const mediaRecorderRef = useRef<MediaRecorder | null>(null);
@@ -38,33 +29,32 @@ export default function DemoPage() {
   const [status, setStatus] = useState("Processing");
   const [isSuccess, setIsSuccess] = useState(false);
   const [isDesktop, setIsDesktop] = useState(false);
-  const [completed, setCompleted] = useState(false);
-  const [transcript, setTranscript] = useState("");
-  const [generatedFeedback, setGeneratedFeedback] = useState("");
 
   useEffect(() => {
     setIsDesktop(window.innerWidth >= 768);
   }, []);
 
   useEffect(() => {
-    if (videoEnded) {
-      const element = document.getElementById("startTimer");
-
-      if (element) {
-        element.style.display = "flex";
-      }
-
-      setCapturing(true);
-
-      mediaRecorderRef.current = new MediaRecorder(
-        webcamRef?.current?.stream as MediaStream
-      );
-      mediaRecorderRef.current.addEventListener(
-        "dataavailable",
-        handleDataAvailable
-      );
-      mediaRecorderRef.current.start();
+    if (!videoEnded) {
+      return;
     }
+    
+    const element = document.getElementById("startTimer");
+
+    if (element) {
+      element.style.display = "flex";
+    }
+
+    setCapturing(true);
+
+    mediaRecorderRef.current = new MediaRecorder(
+      webcamRef?.current?.stream as MediaStream
+    );
+    mediaRecorderRef.current.addEventListener(
+      "dataavailable",
+      handleDataAvailable
+    );
+    mediaRecorderRef.current.start();
   }, [videoEnded, webcamRef, setCapturing, mediaRecorderRef]);
 
   const handleStartCaptureClick = useCallback(() => {
@@ -111,132 +101,7 @@ export default function DemoPage() {
     };
   });
 
-  const handleDownload = async () => {
-    if (recordedChunks.length) {
-      setSubmitting(true);
-      setStatus("Processing");
-
-      const file = new Blob(recordedChunks, {
-        type: `video/webm`,
-      });
-
-      const unique_id = uuid();
-
-      // This checks if ffmpeg is loaded
-      if (!ffmpeg.isLoaded()) {
-        await ffmpeg.load();
-      }
-
-      // This writes the file to memory, removes the video, and converts the audio to mp3
-      ffmpeg.FS("writeFile", `${unique_id}.webm`, await fetchFile(file));
-      await ffmpeg.run(
-        "-i",
-        `${unique_id}.webm`,
-        "-vn",
-        "-acodec",
-        "libmp3lame",
-        "-ac",
-        "1",
-        "-ar",
-        "16000",
-        "-f",
-        "mp3",
-        `${unique_id}.mp3`
-      );
-
-      // This reads the converted file from the file system
-      const fileData = ffmpeg.FS("readFile", `${unique_id}.mp3`);
-      // This creates a new file from the raw data
-      const output = new File([fileData.buffer], `${unique_id}.mp3`, {
-        type: "audio/mp3",
-      });
-
-      const formData = new FormData();
-      formData.append("file", output, `${unique_id}.mp3`);
-      formData.append("model", "whisper-1");
-
-      const question =
-        "Tell me about yourself. Why don${`’`}t you walk me through your resume?";
-      setStatus("Transcribing");
-
-      const upload = await fetch(
-        `/api/transcribe?question=${encodeURIComponent(question)}`,
-        {
-          method: "POST",
-          body: formData,
-        }
-      );
-      const results = await upload.json();
-
-      if (upload.ok) {
-        setIsSuccess(true);
-        setSubmitting(false);
-
-        if (results.error) {
-          setTranscript(results.error);
-        } else {
-          setTranscript(results.transcript);
-        }
-
-        console.log("Uploaded successfully!");
-
-        await Promise.allSettled([
-          new Promise((resolve) => setTimeout(resolve, 800)),
-        ]).then(() => {
-          setCompleted(true);
-          console.log("Success!");
-        });
-
-        if (results.transcript.length > 0) {
-          const prompt = `Please give feedback on the following interview question: ${question} given the following transcript: ${
-            results.transcript
-          }. ${
-            selected.name === "Behavioral"
-              ? "Please also give feedback on the candidate's communication skills. Make sure their response is structured (perhaps using the STAR or PAR frameworks)."
-              : "Please also give feedback on the candidate's communication skills. Make sure they accurately explain their thoughts in a coherent way. Make sure they stay on topic and relevant to the question."
-          } \n\n\ Feedback on the candidate's response:`;
-
-          setGeneratedFeedback("");
-          const response = await fetch("/api/generate", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              prompt,
-            }),
-          });
-
-          if (!response.ok) {
-            throw new Error(response.statusText);
-          }
-
-          // This data is a ReadableStream
-          const data = response.body;
-          if (!data) {
-            return;
-          }
-
-          const reader = data.getReader();
-          const decoder = new TextDecoder();
-          let done = false;
-
-          while (!done) {
-            const { value, done: doneReading } = await reader.read();
-            done = doneReading;
-            const chunkValue = decoder.decode(value);
-            setGeneratedFeedback((prev: any) => prev + chunkValue);
-          }
-        }
-      } else {
-        console.error("Upload failed.");
-      }
-
-      setTimeout(function () {
-        setRecordedChunks([]);
-      }, 1500);
-    }
-  };
+  const SubmitAnswer = () => {};
 
   function restartVideo() {
     setRecordedChunks([]);
@@ -282,7 +147,7 @@ export default function DemoPage() {
                 <div className="relative z-10 h-full w-full rounded-lg">
                   <Timer seconds={seconds} />
                   <InterviewerVideo
-                    setVideoEnded={setVideoEnded}
+                    onVideoEnded={() => setVideoEnded(true)}
                     ref={vidRef}
                   ></InterviewerVideo>
                   <Webcam
@@ -314,7 +179,7 @@ export default function DemoPage() {
                               ></RestartButton>
                             )}
                             <button
-                              onClick={handleDownload}
+                              onClick={SubmitAnswer}
                               disabled={isSubmitting}
                               className="group rounded-full min-w-[140px] px-4 py-2 text-[13px] font-semibold transition-all flex items-center justify-center bg-[#1E2B3A] text-white hover:[linear-gradient(0deg, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0.1)), #0D2247] no-underline flex  active:scale-95 scale-100 duration-75  disabled:cursor-not-allowed"
                               style={{
