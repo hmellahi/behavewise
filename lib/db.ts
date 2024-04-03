1cafb10e353376070327f2d1f8a280cbc2d49d05
Mohamed El omary <elomarymohamed72@gmail.com>
2024-04-03 23:13:22 +0000

    Feat: Setup Prisma and Supabase

---
+++ lib/db.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+export const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prisma;
+}
