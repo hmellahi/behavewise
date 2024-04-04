89488adda1c5d56976ec0ec9cacd951437a57800
hmellahi <hamzasenpai307@gmail.com>
2024-04-04 04:51:09 +0000

    Q/A videos

--- src/app/interview/components/InterviewerVideo.tsx
+++ src/app/interview/components/InterviewerVideo.tsx
@@ -1,31 +1,31 @@
-import { forwardRef } from "react";
+import { interviewQuestion } from "../types/Interview";
 
-const InterviewerVideo = forwardRef(
-  ({ onVideoEnded }: { onVideoEnded: () => void }, ref) => {
-    return (
-      <div className="block absolute top-[10px] sm:top-[20px] lg:top-[40px] left-auto right-[10px] sm:right-[20px] md:right-10 h-[80px] sm:h-[140px] md:h-[180px] aspect-video rounded z-20">
-        <div className="h-full w-full aspect-video rounded md:rounded-lg lg:rounded-xl">
-          <video
-            id="question-video"
-            onEnded={() => onVideoEnded()}
-            controls={false}
-            // @ts-ignore
-            ref={ref}
-            playsInline
-            className="h-full object-cover w-full rounded-md md:rounded-[12px] aspect-video"
-            crossOrigin="anonymous"
-          >
-            <source
-              src="https://liftoff-public.s3.amazonaws.com/DemoInterviewMale.mp4"
-              type="video/mp4"
-            />
-          </video>
-        </div>
+const InterviewerVideo = ({
+  onVideoEnded,
+  question,
+  vidRef,
+}: {
+  onVideoEnded: () => void;
+  question: interviewQuestion;
+  vidRef: React.RefObject<HTMLVideoElement>;
+}) => {
+  return (
+    <div className="block absolute top-[10px] sm:top-[20px] lg:top-[40px] left-auto right-[10px] sm:right-[20px] md:right-10 h-[80px] sm:h-[140px] md:h-[180px] aspect-video rounded z-20">
+      <div className="h-full w-full aspect-video rounded md:rounded-lg lg:rounded-xl">
+        <video
+          id="question-video"          
+          onEnded={() => onVideoEnded()}
+          controls={false}
+          ref={vidRef}
+          playsInline
+          className="h-full object-cover w-full rounded-md md:rounded-[12px] aspect-video"
+          crossOrigin="anonymous"
+        >
+          <source src={question.videoUrl} type="video/mp4" />
+        </video>
       </div>
-    );
-  }
-);
-
-InterviewerVideo.displayName = "InterviewerVideo";
+    </div>
+  );
+};
 
 export default InterviewerVideo;
---
+++ src/app/interview/constants/interviewQuestions.ts
@@ -0,0 +1,26 @@
+import { interviewQuestion } from "../types/Interview";
+
+const interviewQuestions: interviewQuestion[] = [
+  {
+    id: 2,
+    videoUrl:
+      "https://res.cloudinary.com/dv2xxj5vi/video/upload/v1712204779/interview/todpa1qrqw1nyz2522q9.mp4",
+    caption:
+      "Tell me about yourself. Why don't you walk me through your resume?",
+  },
+  {
+    id: 1,
+    videoUrl:
+      "https://res.cloudinary.com/dv2xxj5vi/video/upload/f_auto:video,q_auto/ejqd1ksxpbbf2lwpk7le",
+    caption:
+      "What’s an example of a difficult problem you solved? Be specific about how the problem was diagnosed and your process for approaching it.",
+  },
+  {
+    id: 3,
+    videoUrl:
+      "https://res.cloudinary.com/dv2xxj5vi/video/upload/v1712201664/interview/ypzwd3hdffkrxs4qox7q.mp4",
+    caption: "So, why are you interested in joining our company?",
+  },
+];
+
+export default interviewQuestions;
